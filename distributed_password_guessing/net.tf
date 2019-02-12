6f99caeb0cb9f508220495d44afd2048532c51a4
Ryan <ryan@scalesec.com>
2019-02-12 10:58:27 -0800

    Added really bad security group rule

--- distributed_password_guessing/net.tf
+++ distributed_password_guessing/net.tf
@@ -55,6 +55,13 @@ resource "aws_security_group" "dpg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 5678
+    to_port     = 5678
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port = 0
     to_port   = 0
